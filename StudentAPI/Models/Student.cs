1b545518aadf986c7b8e4d9f907af032f99b62fc
Justin Moore <hametsu777@gmail.com>
2023-09-25 13:50:38 -0400

    Add project files.

---
+++ StudentAPI/Models/Student.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StudentAPI.Models
+{
+    public class Student
+    {
+        [Key]
+        public int StudentId { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(25)")]
+        public string Name { get; set; } = "";
+
+        public string ContactNumber { get; set; }
+        public int Age { get; set; }
+    }
+}
